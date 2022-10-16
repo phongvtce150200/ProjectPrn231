5414fef13f73669e84954619c71f521e70f590fc
Phong Thanh Vo <phongvtce150200@fpt.edu.vn>
2022-10-16 18:35:10 +0700

    Add FE Medicine Manage

---
+++ WebClient/Controllers/MedicineController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class MedicineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
